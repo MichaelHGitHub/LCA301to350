e8de07095580a6ef266f467027ec6e7a47b93441
Kindsword <kindsword@live.com>
2022-08-01 11:55:01 -0400

    Add 303

---
+++ 303_Range_Sum_Immutable/header.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../common/common.h"
+
+void PrepareTestData(vector<TD_VI_I>& testData);
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) :
+        sums(nums.size(), 0)
+    {
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            sums[i] = sums[i - 1] + nums[i];
+        }
+    }
+
+    int sumRange(int left, int right)
+    {
+        if (left > 0)
+        {
+            return sums[right] - sums[left - 1];
+        }
+        else
+        {
+            return sums[right];
+        }
+        
+    }
+
+private:
+    vector<int> sums;
+};
